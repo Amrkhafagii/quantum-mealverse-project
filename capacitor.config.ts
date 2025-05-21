b52fc9573d885c7b60f1717c4df6499588db78c0
amkhafagi <amrkhafagi@live.com>
2025-05-21 03:04:16 +0300

    dsds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,7 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.117bb8e72e6f4681936555049936510d',
   appName: 'quantum-mealverse-project',
   webDir: 'dist',
-  server: {
-    url: 'https://117bb8e7-2e6f-4681-9365-55049936510d.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+ 
   plugins: {
     BiometricAuth: {
       // Plugin-specific configurations would go here
