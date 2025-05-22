9c13ebb14854a00c00f7bd510440e3e5861b440b
amkhafagi <amrkhafagi@live.com>
2025-05-22 16:32:26 +0300

    sss

--- ios/App/App/LocationPermissionsPlugin.m
+++ ios/App/App/LocationPermissionsPlugin.m
@@ -1,13 +1,8 @@
-
 #import <Foundation/Foundation.h>
+#import <Capacitor/Capacitor-Swift.h>
 #import "LocationPermissionsPlugin.h"
-#import <Capacitor/Capacitor.h>
 
-// This is a proper bridge implementation that forwards to Swift
-// instead of providing a duplicate implementation
-@implementation LocationPermissionsPlugin
-
-CAP_PLUGIN_METHOD(checkPermissionStatus, CAPPluginReturnPromise);
-CAP_PLUGIN_METHOD(requestLocationPermission, CAPPluginReturnPromise);
-
-@end
+CAP_PLUGIN(LocationPermissionsPlugin, "LocationPermissions",
+    CAP_PLUGIN_METHOD(checkPermissionStatus, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(requestLocationPermission, CAPPluginReturnPromise);
+)
