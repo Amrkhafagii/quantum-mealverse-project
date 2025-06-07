5f36fb506b6df45499678226499443dd301febc6
amkhafagi <amrkhafagi@live.com>
2025-06-07 23:29:09 +0300

    ddd

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -6182,7 +6182,7 @@ export type Database = {
           subtotal: number
           total: number
           updated_at?: string
-          user_id: string
+          customer_id: string
         }
         Update: {
           accepted_at?: string | null
