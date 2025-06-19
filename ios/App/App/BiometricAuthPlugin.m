4baaef95f144b0ec9fb13a07ef64537198d0437f
amkhafagi <amrkhafagi@live.com>
2025-06-19 13:51:13 +0300

    dwwd

--- ios/App/App/BiometricAuthPlugin.m
+++
@@ -1,9 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(BiometricAuthPlugin, "BiometricAuth",
-           CAP_PLUGIN_METHOD(isAvailable, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(authenticate, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setupBiometricLogin, CAPPluginReturnPromise);
-)
--- ios/App/App/LocationPermissionsPlugin.h
+++
@@ -1,8 +0,0 @@
-
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(LocationPermissionsPlugin, "LocationPermissions",
-           CAP_PLUGIN_METHOD(checkPermissionStatus, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(requestLocationPermission, CAPPluginReturnPromise);
-)
